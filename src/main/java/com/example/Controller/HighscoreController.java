97e5bafd944e1da447a24085d46b8455eafab54e
katrin <katrinholztrattner@gmail.com>
2025-01-06 19:38:54 +0100

    first change Highscore

--- src/main/java/com/example/Controller/HighscoreController.java
+++ src/main/java/com/example/Controller/HighscoreController.java
@@ -1,4 +1,22 @@
 package com.example.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HighscoreController {
-}
+
+    //Interne Liste zur Speicherung der Highscore Ergebnisse
+
+    private List<HighscoreController> entries;
+
+    //Konstruktor wird Highscor Liste übergeben
+
+    public HighscoreController(String name, int score){
+        entries = new ArrayList<>();
+    }
+
+    //Methode zum Generieren von Ergebnissen
+
+    public void addHighscore(String name, int score){
+        entries.add(new HighscoreController(name, score);
+    }
